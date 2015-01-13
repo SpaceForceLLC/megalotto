9f6e41b218f9fc6d1ef720aa99bf0a15aff71751
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-01-13 18:28:58 +0300

    Add draw method and passed tests

--- lib/mega_lotto/drawing.rb
+++ lib/mega_lotto/drawing.rb
@@ -1,7 +1,14 @@
 module MegaLotto
   class Drawing
     def draw
-      Array.new(5)
+      # Original code says 5, but it should be six otherwise rspec does n't allow it!
+      6.times.map { single_draw }
+    end
+
+    private
+
+    def single_draw
+      rand(0..60)
     end
   end
 end
